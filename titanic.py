65a6f3b27d91cd3b4e18408b489d1a3c64ae4138
Ben Graham <bgraham@bgraham.com.au>
2022-01-20 13:11:42 +1100

    Stub functions returning incorrect results
    We're adding these so we have a clear "interface" for the test suite to
    target, and so candidates aren't bamboozled by a totally blank slate.

---
+++ titanic.py
@@ -0,0 +1,25 @@
+"""
+Hello from LivePreso :) This is the file you should be editing. Good luck!
+"""
+
+path = "./titanic.csv"
+
+
+def number_of_passengers():
+    return 900
+
+
+def total_fare_paid():
+    return 30000
+
+
+def median_fare():
+    return 15
+
+
+def cherbourg_survival_rate():
+    return 0.5
+
+
+def surviviest_passenger_class():
+    return [2, 3, 1]
