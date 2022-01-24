578c2716f55a5fd616fa6b48cb3c37e39f03d7df
ibrokemypie <ibrokemypie@outlook.com>
2022-01-24 16:54:36 +1100

    Implement Passenger and Titanic classes Using classes and objects rather than playing with the csv data directly for ease of future extensibility.

--- titanic.py
+++ titanic.py
@@ -2,24 +2,59 @@
 Hello from LivePreso :) This is the file you should be editing. Good luck!
 """
 
-path = "./titanic.csv"
+import csv
 
 
-def number_of_passengers():
-    return 900
+class Passenger:
+    def __init__(self, row_dict: dict[str, str]) -> None:
+        self.id = int(row_dict["PassengerId"])
+        self.survived = bool(row_dict["Survived"])
+        self.p_class = int(row_dict["Pclass"])
+        self.name = row_dict["Name"]
+        self.sex = row_dict["Sex"]
+        self.age = float(row_dict["Age"]) if not row_dict["Age"] == "" else None
+        self.sib_sp = int(row_dict["SibSp"])
+        self.parch = int(row_dict["Parch"])
+        self.ticket = row_dict["Ticket"]
+        self.fare = float(row_dict["Fare"])
+        self.cabin = row_dict["Cabin"]
+        self.embarked = row_dict["Embarked"]
 
 
-def total_fare_paid():
-    return 30000
+class Titanic:
+    def __init__(self, input_file: str) -> None:
+        self.passengers = list[Passenger]()
+
+        with open(input_file, newline="") as file:
+            file_reader = csv.DictReader(file)
+
+            for row_dict in file_reader:
+                self.passengers.append(Passenger(row_dict))
+
+    def print_passengers(self) -> None:
+        for passenger in self.passengers:
+            print(vars(passenger))
+
+    def number_of_passengers(self) -> int:
+        return 900
+
+    def total_fare_paid(self) -> int:
+        return 30000
+
+    def median_fare(self) -> int:
+        return 15
+
+    def cherbourg_survival_rate(self) -> float:
+        return 0.5
+
+    def passenger_class_by_survival(self) -> list[int]:
+        return [2, 3, 1]
 
 
-def median_fare():
-    return 15
+def main():
+    titanic = Titanic("./titanic.csv")
+    titanic.print_passengers()
 
 
-def cherbourg_survival_rate():
-    return 0.5
-
-
-def passenger_class_by_survival():
-    return [2, 3, 1]
+if __name__ == "__main__":
+    main()
