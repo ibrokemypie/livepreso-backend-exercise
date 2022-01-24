a825a3c4329a34c2ec83419d5ae6c95fbf02c244
ibrokemypie <ibrokemypie@outlook.com>
2022-01-24 18:22:41 +1100

    Add additional input checking for Passenger constructor. The changed attributes aren't used in this project, but should be handled correctly anyway for completeness.

--- titanic.py
+++ titanic.py
@@ -10,16 +10,24 @@ import statistics
 
 class Passenger:
     def __init__(self, row_dict: dict[str, str]) -> None:
-        self.id = int(row_dict["PassengerId"])
-        self.survived = bool(int(row_dict["Survived"]))
-        self.p_class = int(row_dict["Pclass"])
+        self.id = (
+            int(row_dict["PassengerId"]) if not row_dict["PassengerId"] == "" else None
+        )
+        self.survived = (
+            bool(int(row_dict["Survived"])) if not row_dict["Survived"] == "" else None
+        )
+        self.p_class = int(row_dict["Pclass"]) if not row_dict["Pclass"] == "" else None
         self.name = row_dict["Name"]
         self.sex = row_dict["Sex"]
-        self.age = float(row_dict["Age"]) if not row_dict["Age"] == "" else None
-        self.sib_sp = int(row_dict["SibSp"])
-        self.parch = int(row_dict["Parch"])
+        self.age = (
+            decimal.Decimal(row_dict["Age"]) if not row_dict["Age"] == "" else None
+        )
+        self.sib_sp = int(row_dict["SibSp"]) if not row_dict["SibSp"] == "" else None
+        self.parch = int(row_dict["Parch"]) if not row_dict["Parch"] == "" else None
         self.ticket = row_dict["Ticket"]
-        self.fare = decimal.Decimal(row_dict["Fare"])
+        self.fare = (
+            decimal.Decimal(row_dict["Fare"]) if not row_dict["Fare"] == "" else None
+        )
         self.cabin = row_dict["Cabin"]
         self.embarked = row_dict["Embarked"]
 
@@ -39,10 +47,22 @@ class Titanic:
         return len(self.passengers)
 
     def total_fare_paid(self) -> float:
-        return float(sum(passenger.fare for passenger in self.passengers))
+        return float(
+            sum(
+                passenger.fare
+                for passenger in self.passengers
+                if passenger.fare is not None
+            )
+        )
 
     def median_fare(self) -> float:
-        return float(statistics.median(passenger.fare for passenger in self.passengers))
+        return float(
+            statistics.median(
+                passenger.fare
+                for passenger in self.passengers
+                if passenger.fare is not None
+            )
+        )
 
     def cherbourg_survival_rate(self) -> float:
         cherbourg_passengers = [
@@ -57,11 +77,15 @@ class Titanic:
 
     def passenger_class_by_survival(self) -> list[int]:
         passengers_by_class = Counter(
-            passenger.p_class for passenger in self.passengers
+            passenger.p_class
+            for passenger in self.passengers
+            if passenger.p_class is not None
         )
 
         survivors_by_class = Counter(
-            passenger.p_class for passenger in self.passengers if passenger.survived
+            passenger.p_class
+            for passenger in self.passengers
+            if passenger.p_class is not None and passenger.survived
         )
 
         # Create a list of tuples containing the class and the percentage of passengers from that class who
